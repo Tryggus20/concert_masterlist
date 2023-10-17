f668e56e229d5f26c247aeabfee81966b397689b
David Campbell <tryggus20@gmail.com>
2023-10-17 09:39:21 -0500

    finally have post route working with everything

--- server/routes/concert.router.js
+++ server/routes/concert.router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
-
-
 // GET request for all users
 router.get("/", (req, res) => {
   const query = `SELECT
@@ -83,18 +81,23 @@ GROUP BY
 
 //                _-_-_-_-_-_-_-_-_- POST REQUEST _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 router.post("/add-concert/:id", async (req, res) => {
-userId = req.params.id;
+  const userId = req.params.id;
   const { venue, city, state, date, bandNames, comment, pictures } = req.body;
+  console.log("req.body for /add-concert/:id", req.body);
 
-  
   try {
     // Check if the bands exist and get their IDs
     const bandIds = [];
     for (const bandName of bandNames) {
-      const result = await pool.query('SELECT id FROM bands WHERE name = $1', [bandName]);
+      const result = await pool.query("SELECT id FROM bands WHERE name = $1", [
+        bandName,
+      ]);
       if (result.rows.length === 0) {
         // Band doesn't exist, so add it and get the id
-        const insertBandResult = await pool.query('INSERT INTO bands (name) VALUES ($1) RETURNING id', [bandName]);
+        const insertBandResult = await pool.query(
+          "INSERT INTO bands (name) VALUES ($1) RETURNING id",
+          [bandName]
+        );
         bandIds.push(insertBandResult.rows[0].id);
       } else {
         //Band does exist, so gt the id
@@ -103,12 +106,15 @@ userId = req.params.id;
     }
 
     // Check if the concert exists
-    const concertResult = await pool.query('SELECT id FROM concerts WHERE venue = $1 AND date = $2', [venue, date]);
+    const concertResult = await pool.query(
+      "SELECT id FROM concerts WHERE venue = $1 AND date = $2",
+      [venue, date]
+    );
     let concertId;
     if (concertResult.rows.length === 0) {
       // Concert doesn't exist, so add it
       const insertConcertResult = await pool.query(
-        'INSERT INTO concerts (venue, city, state, date) VALUES ($1, $2, $3, $4) RETURNING id',
+        "INSERT INTO concerts (venue, city, state, date) VALUES ($1, $2, $3, $4) RETURNING id",
         [venue, city, state, date]
       );
       concertId = insertConcertResult.rows[0].id;
@@ -117,52 +123,35 @@ userId = req.params.id;
     }
 
     // Insert the bands and concert_band relationships
+    let bandConcertIds = []; // Move the declaration outside the loop.
     for (const bandId of bandIds) {
-      await pool.query('INSERT INTO band_concerts (band_id, concert_id) VALUES ($1, $2)', [bandId, concertId]);
-    }
+      const result = await pool.query(
+        "INSERT INTO band_concerts (band_id, concert_id) VALUES ($1, $2) RETURNING id",
+        [bandId, concertId]
+      );
+      const bandConcertId = result.rows[0].id;
+      bandConcertIds.push(bandConcertId);
 
-    // Insert pictures for each band
-    for (const [bandName, urls] of Object.entries(pictures)) {
-      const bandIdResult = await pool.query('SELECT id FROM bands WHERE name = $1', [bandName]);
-      if (bandIdResult.rows.length === 0) {
-        return res.status(400).json({ error: `Band '${bandName}' not found for pictures.` });
-      }
-      const bandId = bandIdResult.rows[0].id;
+      const bandName = bandNames[bandIds.indexOf(bandId)]; // Get band name using the index of bandId in bandIds array
+      const urls = pictures[bandName];
 
       for (const url of urls) {
-        await pool.query('INSERT INTO pictures (user_id, band_concert_id, url) VALUES ($1, $2, $3)', [userId, concertId, url]);
+        await pool.query(
+          "INSERT INTO pictures (user_id, band_concert_id, url) VALUES ($1, $2, $3)",
+          [userId, bandConcertId, url]
+        );
       }
     }
-
     // Insert the comment
-    await pool.query('INSERT INTO user_concerts (user_id, concert_id, comments) VALUES ($1, $2, $3)', [userId, concertId, comment]);
+    await pool.query(
+      "INSERT INTO user_concerts (user_id, concert_id, comments) VALUES ($1, $2, $3)",
+      [userId, concertId, comment]
+    );
 
-    res.status(201).json({ message: 'Concert added successfully.' });
+    res.status(201).json({ message: "Concert added successfully." });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error.' });
+    res.status(500).json({ error: "Internal server error." });
   }
 });
-
-router.post("/", async (req, res) => {
-      const { venue, city, state, date } = req.body;  
-console.log(req.body, "req.body");
-      try {
-        pool.query(`INSERT INTO concerts (venue, city, state, date)
-        VALUES ($1, $2, $3, $4);`, [venue, city, state, date] );
-        res.sendStatus(201)
-      } catch (err) {
-        console.log("error in test", err);
-        res.status(500).json({error: "an error occured in test"})
-      }
-    });
-
-    router.post("/test", async (req, res) => {
-        try{
-           pool.query( `INSERT INTO users (username, password)
-           VALUES ('hi', 'boo' )` )
-    } catch (err) {console.log("error in test", err);
-res.sendStatus(500)}
-} )
-
 module.exports = router;
