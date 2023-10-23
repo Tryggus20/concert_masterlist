031e896414d688ec4c90a49983be86a0cb617c6d
David Campbell <tryggus20@gmail.com>
2023-10-23 16:28:50 -0500

    added env and got server set up to use it

--- server/routes/band.router.js
+++ server/routes/band.router.js
@@ -5,17 +5,14 @@ const router = express.Router();
 // TODO:
 // current object getting passed here only has bandName and index number.
 
-// will need concertID, prev band ID to remove from concert?? 
+// will need band_concert.id
 // Will need to check to see if "new band" exists and if so get that ID. 
 // If not, create new band and add it where need be.
 
 
-// so it would need a PUT, GET, and DELETE all rolled into one with potential to POST 
-// if band does not exist... gross!
-
-/*  Would need to delete from band_concerts, need to add back into band_concerts, 
+/*  
+bands may need a new addition, if so get new ID
+Would need to change band_id in from band_concerts,  
 user_concerts is fine. no need to update.
-bands may need a new addition
-may mess some things up with pictures linking back to band_concerts
 */
 module.exports = router;
